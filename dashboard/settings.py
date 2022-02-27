d6fbc1b2a56da6c4f7dd8603224c23f07930e524
Zeha Irawan <zehairawan@gmail.com>
2022-02-27 16:57:30 +0800

    enable mime type

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
 import django_heroku
+import mimetypes
 
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
