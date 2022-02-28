9c385f6fbc1bfc094530ac8b3c7ec265f27d47c0
Zeha Irawan <zehairawan@gmail.com>
2022-02-28 14:04:02 +0800

    add heysummit logo

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "7c@h1io9=5@8m%fqlyvnx&!x0zm556-g@+dpvu4ab+tsjkm@vm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-DEBUG = False
+# DEBUG = False
 DEBUG_PROPAGATE_EXCEPTIONS=True
 
 ALLOWED_HOSTS = ["*"]
